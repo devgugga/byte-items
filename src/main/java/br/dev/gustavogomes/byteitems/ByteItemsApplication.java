ed428cae95e6d7dc8f21127f3ba49d4254b74d09
Gustavo Gomes <120190910+devgugga@users.noreply.github.com>
2024-03-06 07:25:40 -0300

    :tada: Commit Inicial
    Neste commit foi criado a estrutura inicial do projeto usando o Spring, foi tambem configurado o banco de dados usando o postrgres + docker.

---
+++ src/main/java/br/dev/gustavogomes/byteitems/ByteItemsApplication.java
@@ -0,0 +1,13 @@
+package br.dev.gustavogomes.byteitems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteItemsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ByteItemsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/dev/gustavogomes/byteitems/ByteItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.gustavogomes.byteitems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteItemsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
